b2eac1d49c5b57d599aaee2469772ae44a3f5d36
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2020-09-24 18:33:44 +0500

    flex style fixed in the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,12 +3,10 @@
     <div class="md:self-center">
       <img src="logo.svg" alt="logo">
     </div>
-    <div class="flex justify-center mt-5 md:mt-0">
-      <div class="flex rounded-large border-solid border-2 border-none shadow divide-x-2 divide-gray-100 cursor-pointer">
-        <div class="text-center px-4 py-3">Helsinki, Finland</div>
-        <div class="text-center px-4 py-3">Add guests</div>
-        <div class="text-center px-4 py-3 text-red-500 material-icons">search</div>
-      </div>
+    <div class="flex self-center mt-5 md:mt-0 rounded-large border-solid border-2 border-none shadow divide-x-2 divide-gray-100 cursor-pointer">
+      <div class="px-4 py-3">Helsinki, Finland</div>
+      <div class="px-4 py-3">Add guests</div>
+      <div class="px-4 py-3 text-red-500 material-icons">search</div>
     </div>
   </div>
 </template>
